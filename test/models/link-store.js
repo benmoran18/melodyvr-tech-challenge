89020dad27bab70c06d54fb52f7aacbc19e58f10
Ben Moran <ben.moran@kwiff.com>
2018-08-08 11:28:05 +0100

    Separated routes into matching filenames for easier navigation Moved body parser injection from routes index file to server file Updated link store class to return copy of local store variable Added more tests for link store class

--- test/models/link-store.js
+++ test/models/link-store.js
@@ -1,11 +1,21 @@
 const { expect } = require('chai')
 const { LinkModel, LinkStoreModel} = require('../../lib/models')
+const linkStore = new LinkStoreModel()
+const newLink = new LinkModel().generate('http://www.google.com')
 
 describe('LinkStoreModel Class', () => {
-  it('should store links correctly', () => {
-    const linkStore = new LinkStoreModel()
-    const newLink = new LinkModel().generate('http://www.google.com')
+
+  it('should return undefined if link doesn not exist', () => {
+    expect(linkStore.getLink('some_short_code')).to.be.equal(undefined)
+  })
+
+  it('should store links correctly by shortCode', () => {
     linkStore.addLink(newLink)
     expect(linkStore.getLink(newLink.shortCode)).to.be.equal(newLink)
+  })
+
+  it('should delete links correctly by shortCode', () => {
+    linkStore.deleteLink(newLink.shortCode)
+    expect(linkStore.getLink(newLink.shortCode)).to.be.equal(undefined)
   })
 })
