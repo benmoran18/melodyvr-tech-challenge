25aea592dafd8ceeca910ed2d30addd0b03cad92
Ben Moran <ben.moran@kwiff.com>
2018-08-09 15:24:33 +0100

    Updated link-store test to generate proper link Updated link-store to throw error on timed out links

--- test/models/link-store.js
+++ test/models/link-store.js
@@ -3,10 +3,13 @@ const { LinkFactoryModel, LinkStoreModel} = require('../../lib/models')
 const linkFactoryModel = new LinkFactoryModel()
 const linkStore = new LinkStoreModel()
 const errors = require('../../lib/utils/errors')
+let newLink = null
 
 describe('LinkStoreModel Class', () => {
 
-  const newLink = async () => await linkFactoryModel.generate('http://www.google.com')
+  before(async () => {
+    newLink = await linkFactoryModel.generate('http://www.google.com')
+  })
 
   it('should return undefined if link doesn not exist', async () => {
     try {
@@ -33,14 +36,29 @@ describe('LinkStoreModel Class', () => {
 
   it('should not retrieve link if has reached timeout in config file', async () => {
     const config = require('../../config')
-    config.maximumLinkLifeInMilliseconds = 10
+    config.maximumLinkLifeInMilliseconds = -1
     const LinkStoreModel2 = require('../../lib/models').LinkStoreModel
     let linkStoreModel2 = new LinkStoreModel2()
     try {
       await linkStoreModel2.addLink(newLink)
       await linkStoreModel2.getLink(newLink.shortCode)
+      return false
     } catch (error) {
       expect(error).to.be.equal(errors.CODE_INVALID_SHORT_CODE)
+    }
+  })
+
+  it('should retrieve link if within config timeout', async () => {
+    const config = require('../../config')
+    config.maximumLinkLifeInMilliseconds = 10000000
+    const LinkStoreModel2 = require('../../lib/models').LinkStoreModel
+    let linkStoreModel2 = new LinkStoreModel2()
+    try {
+      await linkStoreModel2.addLink(newLink)
+      let link = await linkStoreModel2.getLink(newLink.shortCode)
+      expect(await linkStore.getLink(newLink.shortCode)).to.be.equal(newLink)
+    } catch (error) {
+      return false
     }
   })
 })
